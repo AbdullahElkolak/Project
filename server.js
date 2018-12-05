2b6796c0902c40ed8b4018ec62ea0e67ceb4bf77
Abdullah Elkolak <abdullahelkolak@gmail.com>
2018-12-05 16:43:13 +0800

    added header

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-const database     =   require('./database');
-const express      =  require('express');
-const bodyparser = require('body-parser');
-var app = express()
+/**
+* Server configuration.
+* Created by Kudzai Gopfa on 3/5/2017.
+* MIT Licensed
+*/
+
+'use strict'
+
+/**
+* Module dependencies
+*/
+
+const database		= require('./database');
+const express		= require('express');
+const bodyParser 	= require('body-parser');
 
 let db = database();
 let app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.set('port', process.env.PORT || 3400);
 
+
 app.listen(app.get('port'), function() {
-    console.log('Server running on PORT: ' + app.get('port') + ' at ' + Date(new Date()));
+	console.log('Server running on PORT: ' + app.get('port') + ' at ' + Date(new Date()));
 });
